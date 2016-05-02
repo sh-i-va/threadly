075b16520d72df33b4ae76538295ca887c164eb9
ShivaPrasad <shiva.prasadv@yahoo.com>
2016-05-02 17:08:14 +0530

    Added some nice animations

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var post = document.getElementById('post');
+var commentsSection = document.getElementById('comments');
+var prevComments = commentsSection.getElementsByTagName('li');
 
 function formSubmit()
 {
     var newComment = document.getElementById('newComment');
-    var commentsSection = document.getElementById('comments');
 
     if(newComment.value != '')
     {
@@ -11,9 +12,24 @@ function formSubmit()
         var commentTextNode = document.createTextNode(newComment.value);
 
         newCommentNode.appendChild(commentTextNode);
+        var offset = newComment.offsetTop - commentsSection.offsetTop + 20;
+        newCommentNode.style.top = offset + 'px';
+
         commentsSection.insertBefore(newCommentNode,commentsSection.firstChild);
         newComment.value = '';
         newComment.blur();
+
+        window.setTimeout(function() {
+            newCommentNode.style.top = '0';
+
+            var prevHeight = 0;
+            var ii;
+
+            for(ii = 1; ii < prevComments.length; ii++) {
+                prevHeight += prevComments[ii-1].clientHeight + 36;
+                prevComments[ii].style.top = prevHeight + 'px';
+            }
+        }, 1);
     }
 
     return false;
